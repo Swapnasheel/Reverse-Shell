6df7fe93bbf2332a801797c98ad6e7fa61f99d90
Swapnasheel <swar.1318@gmail.com>
2018-03-24 03:37:44 +0000

    Added the bind and create socket methods #2

--- src/server.py
+++ src/server.py
@@ -15,5 +15,28 @@ def create_socket():
     except socket.error as err:
         print("Couldn't create socket due to: "+ str(err))
 
-
+# Bind the socket to the host and the port
+def bind_socket():
         
+    try:
+        global hostname
+        global port
+        global conn
+        print ("Binding host: {} with port: {}".format(str(host), str(port)))
+        conn.bind((host, port))
+        conn.listen(5)
+    except socket.error as err:
+        print ("Couldn't bind the socket due to: {}".format(str(err)))
+        print ("Retrying...")
+        bind_socket()
+
+# Establish connection with the client
+def accept_connection():
+
+    try:
+        conn, add = conn.accept()
+        print ("Connection established with host: {} on port: {}".format(add[0], add[1]))
+        send_commands(conn)
+        conn.close()
+
+
